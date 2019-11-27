ac531c96665bd6cbda6f1e3406a22d472589170a
Harry Tate <harry.tate@managers.org.uk>
2019-11-27 22:03:52 +0000

    Services page adding content

--- dist/PeopleProjectLifestyle.e31bb0bc.js
+++ dist/PeopleProjectLifestyle.e31bb0bc.js
@@ -18012,7 +18012,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54851" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59599" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
